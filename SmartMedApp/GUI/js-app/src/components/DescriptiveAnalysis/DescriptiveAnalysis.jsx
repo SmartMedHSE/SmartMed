4cd9ce731bfe6feb897b094e6b5a7fc4b7ecba72
Andrey Prokopenko <prokopenko.rower@mail.ru>
2021-10-31 19:41:30 +0300

    [front] added two more pages to DescriptiveAnalysis

--- SmartMedApp/GUI/js-app/src/components/DescriptiveAnalysis/DescriptiveAnalysis.jsx
+++ SmartMedApp/GUI/js-app/src/components/DescriptiveAnalysis/DescriptiveAnalysis.jsx
@@ -3,11 +3,43 @@ import * as React from 'react';
 import { DataDownload } from '../DataDownload';
 import { ListButtons } from '../ManageButtons';
 import { DataPreparation } from './DataPreparation.jsx';
+import { Checkbox, FormControlLabel, FormGroup } from '@material-ui/core';
 
 const DATA_PREP_OPTIONS = [ 'Средним/модой (численные/категориальные значения)',
     'Введенным значением (требуется ввод для каждого столбца отдельно)',
     'Удаление строк с пропущенными значениями',
     'Медианной/модой (численные/категориальные значения)'
+];
+
+const METRICS = [
+    'Объем выборки',
+    'Среднее по столбцу',
+    'Стандартное отклонение по столбцу',
+    'Максимальное значение в столбце',
+    'Минимальное значение в столбце',
+    'Квантили 25, 50, 75',
+];
+
+const Page = (props) => (
+    <div>
+        <h2>{props.title}</h2>
+        <div>{props?.description || ''}</div>
+        {props.children}
+    </div>
+);
+
+const GRAPH_TYPE = [
+    'Матрица корреляций (в виде диаграммы рассеяния)',
+    'Гистограмма / столбцовая диаграмма',
+    'Матрица корреляций (в численном виде)',
+    'Тепловая карта',
+    'Точечная диаграмма ',
+    'График линейной зависимости',
+    'График ящик с усами (диаграмма размаха)',
+    'Столбцовая диаграмма',
+    'Круговая диаграмма',
+    'График логарифмической зависимости',
+    'Множественная гистограмма',
 ];
 
 export class DescriptiveAnalysis extends React.Component {
@@ -42,6 +74,32 @@ export class DescriptiveAnalysis extends React.Component {
                 return <DataDownload onLoad={() => null}/>;
             case 1:
                 return <DataPreparation onClick={this.selectDataPrepType} options={DATA_PREP_OPTIONS}/>;
+            case 2:
+                return (
+                    <Page
+                        title="Метрики"
+                        description="Выбор статистических метрик"
+                    >
+                        <FormGroup>
+                            {METRICS.map((item, idx) => (
+                                <FormControlLabel key={`m-${item}`} control={<Checkbox/>} label={item}/>
+                            ))}
+                        </FormGroup>
+                    </Page>
+                );
+            case 3:
+                return (
+                    <Page
+                        title="Визуаизация"
+                        description="Выбор графиков для реализации"
+                    >
+                        <FormGroup>
+                            {GRAPH_TYPE.map((item, idx) => (
+                                <FormControlLabel key={`g-${item}`} control={<Checkbox/>} label={item} />
+                                ))}
+                        </FormGroup>
+                    </Page>
+                );
             default:
                 console.error(`unknown page: ${currentPage}`);
         }
