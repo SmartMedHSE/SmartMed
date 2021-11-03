8a8006888e3d25c8213c21dbe15b147f8142e64d
Andrey Prokopenko <prokopenko.rower@mail.ru>
2021-11-04 00:17:19 +0300

    [front] added network module and fixed fonts

--- SmartMedApp/GUI/js-app/src/components/DataDownload/DataDownload.jsx
+++ SmartMedApp/GUI/js-app/src/components/DataDownload/DataDownload.jsx
@@ -5,24 +5,21 @@ import { Button, Input } from '@material-ui/core';
 
 import * as s from './DataDownload.scss';
 
-const onLoad = () => {
-};
-
 /**
- * @param props {{onLoad: (data) => void}}
+ * @param props {{onLoad: (filePath) => void}}
  * @returns {JSX.Element}
  * @constructor
  */
 export const DataDownload = (props) => {
     return (
-        <div class={s.dataDownload}>
-            <h2 class={cn(s.dataDownload__title)}>Загрузите данные</h2>
+        <div className={s.dataDownload}>
+            <h2 className={cn(s.dataDownload__title)}>Загрузите данные</h2>
             <div>
                 Для того, чтобы выполнить загрузку данных, кликните на кнопку ниже.
                 В открывающемся окне выберите файл в формате "xlsx", "csv", "tsv"
             </div>
             <div>
-                <Input multiple type="file">Загрузить</Input>
+                <Input multiple type="file" onChange={props.onLoad}>Загрузить</Input>
                 {/*<Button variant="contained" component="span">*/}
                 {/*    Загрузить*/}
                 {/*</Button>*/}
