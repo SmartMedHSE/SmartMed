d6e8f36aa07442220e4932797176061d4ebdc4fb
LinDel-1 <94544946+LinDel-1@users.noreply.github.com>
2021-12-05 20:00:06 +0300

    back: Add POST preprocessing.
    back: Add POST preprocessing.

--- SmartMedApp/backend/ModuleManipulator.py
+++ SmartMedApp/backend/ModuleManipulator.py
@@ -16,7 +16,6 @@ class ModuleManipulator:
         if self.settings['MODULE'] == 'STATS':
             module = StatisticsModule(self.settings['MODULE_SETTINGS'])
         elif self.settings['MODULE'] == 'PREDICT':
-            print(self.settings['MODULE_SETTINGS'])
             module = PredictionModule(self.settings['MODULE_SETTINGS'])
         elif self.settings['MODULE'] == 'BIOEQ':
             module = BioequivalenceModule(self.settings['MODULE_SETTINGS'])
