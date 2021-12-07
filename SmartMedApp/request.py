edf3a0417a5f2f7c5e8ed044e1638449f66700cd
LinDel-1 <eilitvinenko@edu.hse.ru>
2021-12-07 22:44:00 +0300

    back: Preprocessing POST requst

--- SmartMedApp/request.py
+++ SmartMedApp/request.py
@@ -14,60 +14,55 @@ app = Flask(__name__)
 
 @app.route('/api/descriptive', methods=['POST'])
 def requesting():
+    json_request = None
     if request.method == 'POST':
-        data_json = request.json
+        json_request = request.json
 
-
-    json_example = {
+    base_response = {
         "MODULE_SETTINGS": 
         {"metrics": 
-            {"count": None, "mean": None, "std": None, "max": None, "min": None, "25%": None, 
-            "50%": None, "75%": None}, 
+            {"count": False, "mean": False, "std": False, "max": False, "min": False, "25%": False, 
+            "50%": False, "75%": False}, 
         "graphs": 
-                {"scatter": None, "hist": None, "corr": None, "heatmap": None, "dotplot": None, "linear": None,"box": None,  
-                "piechart": None, "log": None, "multihist": None},
+                {"scatter": False, "hist": False, "corr": False, "heatmap": False, "dotplot": False, "linear": False,"box": False,  
+                "piechart": False, "log": False, "multihist": False},
         "data": 
                 {"preprocessing": 
                     {"fillna": None, "encoding": "label_encoding", "scaling": False},
                 "path": None, "fillna": None}}, 
-        "MODULE": None}
+        "MODULE": "STATS"}
 
 
-
-    data_json['graphs'] = data_json.pop('graphics')
-    data_json['data'] = {
-        'preprocessing': {
-            "fillna": ["mean", "exact_value", 'dropna','median'][data_json['dataPrepOption']],
-            "encoding": "label_encoding", 
-            "scaling": bool(data_json.pop('dataPrepOption'))}, 
-        'path': data_json.pop('data')
-        }
-
-    data_json['data']['fillna'] = data_json['data']['preprocessing']['fillna']
-    data_json['MODULE_SETTINGS'] = {
-        'metrics': data_json.pop('metrics'),
-        'graphs': data_json.pop('graphs'), 
-        'data': data_json.pop('data')
-        }
-    data_json['MODULE'] = 'STATS' 
-    #Перебираем ключи, значения для которых нужно изменить
-    for settings in ['metrics', 'graphs']:
-        #Сохряняем пользв. выбор в отдельную переменную, отсортировав по порядку
-        tmp_lst = sorted(data_json["MODULE_SETTINGS"][settings])
-        data_json["MODULE_SETTINGS"][settings] = json_example["MODULE_SETTINGS"][settings]
-        #Словарь ключей settings
-        list_of_keys = list(json_example["MODULE_SETTINGS"][settings].keys())
-        for sett in tmp_lst:
-            if sett == 5 and settings == 'metrics':
-                data_json["MODULE_SETTINGS"][settings]["25%"] = True
-                data_json["MODULE_SETTINGS"][settings]["50%"] = True
-                data_json["MODULE_SETTINGS"][settings]["75%"] = True
-                continue
-            #Отмечаем выбранные пользователем параметры
-            data_json["MODULE_SETTINGS"][settings][list_of_keys[sett]] = True
+    json_request["graphs"] = json_request.pop("graphics") #Замена на одноименный ключ в шаблонном словаре
+    base_settings = base_response["MODULE_SETTINGS"] #Замена переменной для читаемости кода
 
 
-    module_starter = ModuleManipulator(data_json)
+    for key_from_response in json_request:
+        if key_from_response == 'data': #Присваиваем значение путя к файлу
+            base_settings['data']['path'] = json_request['data']
+
+
+        elif key_from_response == 'dataPrepOption':#Присваиваем значение предобработки из файла
+            list_of_prep = ["mean", "exact_value", 'dropna','median'] #Список способов предобработки
+            choose_of_prep = json_request['dataPrepOption'] #Индекс из списка способов предобработки
+            base_settings['data']['preprocessing']['fillna'] = list_of_prep[choose_of_prep]
+            base_settings['data']['fillna'] = list_of_prep[choose_of_prep]
+
+
+        elif key_from_response in ['metrics','graphs']: #Перебираем выбранные метрики пользователем
+            #Массив из значений ключей метрик или графиков шаблонного запроса
+            key_of_metrics = list(base_settings[key_from_response].keys())
+            for user_metrics in sorted(json_request[key_from_response]): #Перебираем значения метрик или графиков пользователя и выбираем их в шаблонном запросе
+                #Т.е. выбираем ключ из ключей метрик или графиков, соотвествующий номеру из запроса и присваиваем ему True
+                tmp_name_of_metric = key_of_metrics[user_metrics]
+                base_settings[key_from_response][tmp_name_of_metric] = True
+        
+    if base_settings['metrics']['25%']: #Если выбран квантиль 25%, то за ним выбираются 50 и 75
+        base_settings['metrics']['50%'] = True
+        base_settings['metrics']['75%'] = True
+
+    print(base_response)
+    module_starter = ModuleManipulator(base_response)
     threading.Thread(target=module_starter.start, daemon=True).start()
     return 'get'
 
