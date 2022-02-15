51cd5c3ba2033768d4cffcae5591cad539c32f53
Egor <egormarusev@gmail.com>
2022-02-16 02:29:26 +0300

    Fixed text in GUI

--- SmartMedApp/GUI/apps/PredictionApp/TreeVisualWindow.py
+++ SmartMedApp/GUI/apps/PredictionApp/TreeVisualWindow.py
@@ -18,10 +18,10 @@ class TreeVisualWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(70, 30, 521, 81))
+        self.label.setGeometry(QtCore.QRect(70, 30, 521, 51))
         self.label.setObjectName("label")
         self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(110, 140, 600, 200))
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(110, 100, 600, 200))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
@@ -55,7 +55,7 @@ class TreeVisualWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt;\">Выберите таблицы и графики </span></p><p><span style=\" font-size:18pt;\"></span></p></body></html>"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:17pt;\">Выберите таблицы и графики:</span></p></body></html>"))
         self.checkBoxTree.setText(_translate("MainWindow", "  Графическое преставление дерева"))
         self.checkBoxTablr.setText(_translate("MainWindow", "  Классификационная таблица, в которой наблюдаемые показатели\n  "
 "противопоставляются предсказанным "))
